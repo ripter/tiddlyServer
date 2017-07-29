9831bf9463835180d23faaebf528e761c8e31723
Chris Richards <ripter001@gmail.com>
2017-07-29 15:56:45 -0700

    the routes are working and returning.

---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+let globalRevision = 1;
+
+// Setup static file server for the index.html
+app.use(express.static(path.join(__dirname, 'public')));
+// The page will send us JSON back.
+app.use(bodyParser.json());
+
+app.get('/status', function(req, res) {
+  res.json({"space":{"recipe":"default"},"tiddlywiki_version":"5.1.14"});
+});
+
+app.get('/recipes/default/tiddlers.json', function(req, res) {
+  res.json({});
+});
+
+app.put('/recipes/default/tiddlers/:title', function(req, res) {
+  const title = encodeURIComponent(req.params.title);
+  const revision = globalRevision++;
+  console.log('PUT: /recipes/default/tiddlers/:title');
+  console.log('\treq.params', req.params);
+  console.log('\treq.body', req.body);
+  res.set('Etag', `"default/${title}/${revision}:"`);
+  res.sendStatus(200);
+});
+
+app.delete('/bags/:bag/tiddlers/:title', function(req, res) {
+  console.log('DELETE: /bags/:bag/tiddlers/:title');
+  console.log('\treq.params', req.params);
+  console.log('\treq.body', req.body);
+  res.sendStatus(204);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
